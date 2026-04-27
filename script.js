c6b8177cea7b71076f331beabe3dd38e28f7f50a
Serj Tankian <phmundel2009@gmail.com>
2026-04-27 08:59:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const toggle = document.getElementById("menu-toggle")
-const menu = document.getElementById("menu")
-const overlay = document.getElementById("overlay")
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
 
-toggle.addEventListener("click",() => {
+toggle.addEventListener("click", () => {
     menu.classList.toggle("active");
     overlay.classList.toggle("active");
 })
 
-overlay.addEventListener("click",() => {
+overlay.addEventListener("click", () => {
     menu.classList.remove("active");
     overlay.classList.remove("active");
 })
+
